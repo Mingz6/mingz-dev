eecdb64346d5cb27da3387ac3ac24858806f2642
Ming <Mingz6@outlook.com>
2026-04-07 00:10:49 -0700

    fix: replace Astro rocket with MZ monogram logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
-import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
