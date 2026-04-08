9f3e13942cea92979918441b42f0050556a75f27
Ming <Mingz6@outlook.com>
2026-04-08 13:38:24 -0700

    fix: replace template README, remove template images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import mdx from "@astrojs/mdx"
 import solidJs from "@astrojs/solid-js"
 import tailwind from "@astrojs/tailwind"
-import rehypeExternalLinks from "rehype-external-links"
 import { defineConfig } from "astro/config"
+import rehypeExternalLinks from "rehype-external-links"
 
 // https://astro.build/config
 export default defineConfig({
